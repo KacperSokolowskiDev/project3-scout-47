c0a640aee02e257233813a2024755f70e906c4ba
Greg155 <gregakhun@gmail.com>
2020-12-18 15:11:03 +0100

    update end sprint 2

--- web-app/src/components/Criteria.js
+++ web-app/src/components/Criteria.js
@@ -1,6 +1,7 @@
 import React from "react";
+import "./MainCss.css";
 
 function Criteria() {
-  return <h1>SECTION CRITÈRES 🧮</h1>;
+  return <h1 className="pageTitle">SECTION CRITÈRES 🧮</h1>;
 }
 export default Criteria;
--- web-app/src/components/Navigation.js
+++ web-app/src/components/Navigation.js
@@ -1,29 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./MainCss.css";
+import logoScout47 from "../assets/logo_scout47.png";
 
 function Navigation() {
   return (
-    <ul style={{ display: "flex" }}>
-      <Link to="/">
-        <li style={{ marginLeft: 10, listStyle: "none" }}>Profil</li>
-      </Link>
+    <div className="NavBar">
+      <div className="logo">
+        <img className="logoScout47" src={logoScout47} alt="y en a marre" />
+      </div>
+      <div className="list">
+        <ul className="link_Wrapper">
+          <Link className="navigation" to="/">
+            <li className="list_item"> Profil |</li>
+          </Link>
 
-      <Link to="/Rapport">
-        <li style={{ marginLeft: 10, listStyle: "none" }}>Rapport</li>
-      </Link>
+          <Link className="navigation" to="/Rapport">
+            <li className="list_item"> Rapport |</li>
+          </Link>
 
-      <Link to="/Criteria">
-        <li style={{ marginLeft: 10, listStyle: "none" }}>Criteria</li>
-      </Link>
+          <Link className="navigation" to="/Criteria">
+            <li className="list_item"> Criteria |</li>
+          </Link>
 
-      <Link to="/Agenda">
-        <li style={{ marginLeft: 10, listStyle: "none" }}>Agenda</li>
-      </Link>
+          <Link className="navigation" to="/Agenda">
+            <li className="list_item"> Agenda |</li>
+          </Link>
 
-      <Link to="/AddPlayer">
-        <li style={{ marginLeft: 10, listStyle: "none" }}>AddPLayer</li>
-      </Link>
-    </ul>
+          <Link className="navigation" to="/AddPlayer">
+            <li className="list_item"> AddPLayer |</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
   );
 }
 
