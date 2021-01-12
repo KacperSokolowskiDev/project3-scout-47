a08a12428ffa985ebbac850b2073b6e8cdab78b6
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-01-12 15:16:10 +0100

    I tried but this calendar is annoying af

--- web-app/src/pages/agenda/Index.js
+++ web-app/src/pages/agenda/Index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/navbar/Index";
 import LateralBar from "../../components/LateralBar/Index";
+import EventCalendar from "../../components/EventCalendar/Index";
 import "./styles.css";
 
 function Index() {
@@ -11,7 +12,6 @@ function Index() {
         <LateralBar />
         <div className="agenda-page-content">
           <h1 className="agenda-title">AGENDA</h1>
-          <h2 className="agenda-title">test</h2>
         </div>
       </div>
     </div>
