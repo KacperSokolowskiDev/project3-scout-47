b954892558e638d2ecc976d53f6e080b2fae2b5f
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-01-12 14:18:04 +0100

    started working on the web agenda page

--- web-app/src/pages/agenda/Index.js
+++ web-app/src/pages/agenda/Index.js
@@ -5,9 +5,15 @@ import "./styles.css";
 
 function Index() {
   return (
-    <div className="page-container">
-      <LateralBar />
+    <div className="agenda-page">
       <Navbar />
+      <div className="agenda-page-container">
+        <LateralBar />
+        <div className="agenda-page-content">
+          <h1 className="agenda-title">AGENDA</h1>
+          <h2 className="agenda-title">test</h2>
+        </div>
+      </div>
     </div>
   );
 }
