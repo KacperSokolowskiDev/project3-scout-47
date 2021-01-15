fde4550ec0eba4387cb7cbc158b985400393fe05
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-01-15 10:03:34 +0100

    Updated Web-app

--- web-app/src/pages/staff/Index.js
+++ web-app/src/pages/staff/Index.js
@@ -1,15 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import { Fab, TextField, Tooltip } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import { makeStyles } from "@material-ui/core/styles";
+
 import Navbar from "../../components/navbar/Index";
 import LateralBar from "../../components/LateralBar/Index";
-import "./styles.css";
+import StaffCard from "../../components/StaffCard/Index";
 
-function Index() {
+import "./styles.css";
+require("dotenv").config();
+
+const useStyles = makeStyles((theme) => ({
+  buttons: {
+    marginLeft: theme.spacing(2),
+    marginTop: theme.spacing(4),
+  },
+  elementMR: {
+    marginRight: theme.spacing(1),
+  },
+  elementML: {
+    marginLeft: theme.spacing(2),
+  },
+  elementMT: {
+    marginTop: theme.spacing(4),
+  },
+}));
+
+const Index = () => {
+  const classes = useStyles();
+  const [listStaff, setListStaff] = useState([]);
+  const [download, setDownload] = useState(false);
+
+  const fetchStaff = async () => {
+    await axios
+      .get("http://localhost:5000/api/scouts")
+      .then((res) => {
+        let result = res.data;
+        setListStaff(result);
+        setDownload(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchStaff();
+  }, []);
+
   return (
-    <div className="page-container">
-      <LateralBar />
+    <div className="staff-page">
       <Navbar />
+      <div className="staff-page-container">
+        <LateralBar />
+        <div className="staff-page-content">
+          <h1 className="staff-title">PERSONNELS</h1>
+          <div className="staff-page-search-info">
+            <TextField
+              autoComplete="off"
+              color="secondary"
+              id="filled-basic"
+              label="Recherche"
+              variant="filled"
+              className={classes.elementMT}
+            />
+            <Tooltip title="Add" aria-label="add">
+              <Fab color="secondary" className={classes.buttons}>
+                <AddIcon />
+              </Fab>
+            </Tooltip>
+            <Tooltip title="Filter" aria-label="filter">
+              <Fab color="secondary" className={classes.buttons}>
+                <FilterListIcon />
+              </Fab>
+            </Tooltip>
+          </div>
+          <div className="staff-page-list">
+            {download ? (
+              listStaff.map((data) => {
+                return <StaffCard staffInfo={data} />;
+              })
+            ) : (
+              <div>No staff in database</div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Index;
