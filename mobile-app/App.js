e418e6aecbd8ae616075a0c557c69de13d571110
YanisLem <yanislemfadli@gmail.com>
2021-01-19 16:46:03 +0100

    update agenda

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -2,50 +2,6 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import AuthNavigator from "./app/navigation/AuthNavigator";
-import { LocaleConfig } from "react-native-calendars";
-
-LocaleConfig.locales["fr"] = {
-  monthNames: [
-    "Janvier",
-    "Février",
-    "Mars",
-    "Avril",
-    "Mai",
-    "Juin",
-    "Juillet",
-    "Août",
-    "Septembre",
-    "Octobre",
-    "Novembre",
-    "Décembre",
-  ],
-  monthNamesShort: [
-    "Janv.",
-    "Févr.",
-    "Mars",
-    "Avril",
-    "Mai",
-    "Juin",
-    "Juil.",
-    "Août",
-    "Sept.",
-    "Oct.",
-    "Nov.",
-    "Déc.",
-  ],
-  dayNames: [
-    "Dimanche",
-    "Lundi",
-    "Mardi",
-    "Mercredi",
-    "Jeudi",
-    "Vendredi",
-    "Samedi",
-  ],
-  dayNamesShort: ["Dim.", "Lun.", "Mar.", "Mer.", "Jeu.", "Ven.", "Sam."],
-  today: "Aujourd'hui",
-};
-LocaleConfig.defaultLocale = "fr";
 
 const Stack = createStackNavigator();
 
