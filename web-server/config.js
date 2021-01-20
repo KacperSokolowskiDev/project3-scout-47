cd8fdb128d6ce71d28ae6a6cadd07532a17bfd23
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-01-20 17:41:37 +0100

    Added Admin Bro in the web-server and started working on the dasboard page and staffProfilCard element

--- web-server/config.js
+++ web-server/config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 const { Sequelize } = require("sequelize");
+
 const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
